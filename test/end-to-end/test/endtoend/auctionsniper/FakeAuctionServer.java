fd38ddd972f885868c32aee45dd368d9ef7de296
KangYong <arahansa@naver.com>
2015-02-10 13:01:41 +0900

    problem solved.

--- test/end-to-end/test/endtoend/auctionsniper/FakeAuctionServer.java
+++ test/end-to-end/test/endtoend/auctionsniper/FakeAuctionServer.java
@@ -83,11 +83,13 @@ public class FakeAuctionServer {
 	
 	public class SingleMessageListener implements MessageListener{
 		private final ArrayBlockingQueue<Message> messages = new ArrayBlockingQueue<Message>(1);
+		@Override
 		public void processMessage(Chat chat, Message message){
 			messages.add(message);
 		}
 		
 		public void receivesAmessage(Matcher<? super String> messageMatcher) throws InterruptedException{
+						Thread.sleep(1000);
 						final Message message = messages.poll(5, TimeUnit.SECONDS);
 						assertThat("Message", message, is(notNullValue()));
 						assertThat(message.getBody(), messageMatcher);
